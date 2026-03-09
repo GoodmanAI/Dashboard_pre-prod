ad93a46abab82167805aaf3275f82fa85c6bf014
LAGEISTE David <lageiste.david@gmail.com>
2026-03-09 16:37:23 +0100

    fix route

--- src/app/api/calls/[id]/treated/route.ts
+++ src/app/api/calls/[id]/treated/route.ts
@@ -3,23 +3,20 @@ import { NextResponse } from "next/server";
 import { getIO } from "@/lib/socket";
 
 export async function PATCH(req: Request, { params }: any) {
+
   const id = Number(params.id);
   const { treated } = await req.json();
 
-  console.log("id", id);
-
-  const io = getIO();
-  
-  io.emit("call-treated", {
-    callId: id,
-    treated: treated
-  });
-
   const call = await prisma.callConversation.update({
     where: { id },
-    data: {
-      treated,
-    },
+    data: { treated },
+  });
+
+  const io = getIO();
+
+  io.emit("call-treated", {
+    callId: id,
+    treated
   });
 
   return NextResponse.json(call);
---
+++ src/pages/api/socket.ts
@@ -0,0 +1,24 @@
+import { Server } from "socket.io";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { setIO } from "@/lib/socket";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+
+  if (!(res.socket as any).server.io) {
+
+    console.log("Initialisation Socket.io");
+
+    const io = new Server((res.socket as any).server);
+
+    (res.socket as any).server.io = io;
+
+    setIO(io);
+
+    io.on("connection", (socket) => {
+      console.log("client connecté", socket.id);
+    });
+
+  }
+
+  res.end();
+}
