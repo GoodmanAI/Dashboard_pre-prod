002d839fc62ac8b82725770413144efc268a1c52
LAGEISTE David <lageiste.david@gmail.com>
2026-03-10 22:55:14 +0100

    remove log

--- src/app/client/services/talk/[id]/calls/page.tsx
+++ src/app/client/services/talk/[id]/calls/page.tsx
@@ -230,7 +230,6 @@ export default function CallListPage({ params }: CallListPageProps) {
 
         const { data, total } = await res.json();
 
-        console.log("data", data);
         setCalls(data);
         setTotal(total);
         const initialCheckbox: Record<number, boolean> = {};
